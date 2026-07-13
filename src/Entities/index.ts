464ca159ca83bbfff1b215261bc880d199d17409
EliasF1912 <e.duvernois2016@gmail.com>
2026-07-13 16:23:38 +0200

    Ajout de la connection base de donnée PostgreSQL

---
+++ src/Entities/index.ts
@@ -0,0 +1,7 @@
+export * from "./artist-entity";
+export * from "./artwork-entity";
+export * from "./artworkStatusHistory-entity";
+export * from "./exhibition-entity";
+export * from "./loan-entity";
+export * from "./sale-entity";
+export * from "./user-entity";
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User, Artist, Loan, Sale, Exhibition, Artwork, ArtworkStatusHistory } from "./Entities"
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal : true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports : [ConfigModule],
+      inject : [ConfigService],
+      useFactory : (config : ConfigService) => ({
+        type : "postgres",
+        host : config.getOrThrow<string>('DB_HOST'),
+        port : config.getOrThrow<number>('DB_PORT'),
+        username: config.getOrThrow<string>('DB_USERNAME'),
+        password: config.getOrThrow<string>('DB_PASSWORD'),
+        database: config.getOrThrow<string>('DB_DATABASE'),
+        entities: [],
+        synchronize: true,
+        logging: true,
+      })
+    })
+  ],
 })
 
 export class AppModule {}
