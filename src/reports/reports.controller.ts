8b1d637a298295baf2ab5702dc92c9dc037190cb
Lytween Victoire <victoire.lytween@gmail.com>
2026-08-30 00:41:13 +0200

    feat(reports): add reports module with gallery, artist and admin stats

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { User } from '../common/decorators/user.decorator';
+import { Roles } from '../common/decorators/role.decorator';
+import { UserRole } from '../common/enum';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private readonly reportsService: ReportsService) { }
+
+    @Get('artist')
+    @Roles(UserRole.ARTIST)
+    getArtistStats(@User() user: any) {
+        return this.reportsService.getArtistStats(user.id);
+    }
+    @Get('gallery')
+    @Roles(UserRole.GALLERY)
+    getGalleryStats(@User() user: any) {
+        return this.reportsService.getGalleryStats(user.id);
+    }
+
+    @Get('admin')
+    @Roles(UserRole.ADMIN)
+    getAdminStats() {
+        return this.reportsService.getAdminStats();
+    }
+}
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Sale } from '../Entities/sale-entity';
+import { User } from '../Entities/user-entity';
+import { Artwork } from '../Entities/artwork-entity';
+import { Artist } from '../Entities/artist-entity';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Sale, User, Artwork, Artist])],
   providers: [ReportsService],
   controllers: [ReportsController]
 })
